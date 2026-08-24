d5d07c8b6fa357cd90a3e99903ee6ba7c9d4824f
biloo dev <billal.20113@gmail.com>
2026-08-24 18:19:10 +0100

    Set app config default port to 10000

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('app', () => ({
   nodeEnv: process.env.NODE_ENV || 'development',
-  port: parseInt(process.env.PORT || '3000', 10),
+  port: parseInt(process.env.PORT || '10000', 10),
   apiPrefix: process.env.API_PREFIX || 'api/v1',
   corsOrigins: process.env.CORS_ORIGINS ? process.env.CORS_ORIGINS.split(',') : '*',
 }));
